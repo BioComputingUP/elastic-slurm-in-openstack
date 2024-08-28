30a71fe66b07d4e9c645109200ce2a4ac366dfee
Ivan Mičetić <ivan.micetic@gmail.com>
2024-08-28 17:34:19 +0200

    Use ansible<10

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,6 +4,6 @@ python3 -m venv ansible
 source ./ansible/bin/activate
 
 pip install --upgrade pip wheel
-pip install --upgrade ansible python-openstackclient openstacksdk==0.52.0
+pip install --upgrade 'ansible<10' python-openstackclient openstacksdk==0.52.0
 
 ansible-galaxy install -r requirements.yml
