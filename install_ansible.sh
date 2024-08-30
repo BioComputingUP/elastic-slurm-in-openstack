2b07e1acd88e3b22a0b27f2778a12c253da97c92
Alberto Gallo <154282660+albygallo@users.noreply.github.com>
2024-08-30 09:24:40 +0200

    ansible 9 requirements

--- install_ansible.sh
+++ install_ansible.sh
@@ -4,6 +4,15 @@ python3 -m venv ansible
 source ./ansible/bin/activate
 
 pip install --upgrade pip wheel
-pip install --upgrade 'ansible<10' python-openstackclient openstacksdk==0.52.0
+pip install --upgrade \
+  ansible==9.2.0 \
+  ansible-core==2.16.3 \
+  python-openstackclient==5.4.0 \
+  openstacksdk==0.52.0 \
+  python-cinderclient==9.4.0 \
+  python-keystoneclient==5.3.0 \
+  python-novaclient==18.4.0 \
+  python-openstackclient==5.4.0 \
+  keystoneauth1==5.5.0
 
 ansible-galaxy install -r requirements.yml
